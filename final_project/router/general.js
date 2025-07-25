6d6802f2994a8d375a8126244988009b627b2f13
jlawins3811 <jlathegodsson@gmail.com>
2025-07-24 22:53:46 -0400

    Completed Task 13: Implemented get book details by title using async-await with Axios

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -1,10 +1,13 @@
 const express = require('express');
-let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
-
-
+const axios = require('axios');
+let books = {
+    "1": { "title": "Fairy tales", "author": "Author A", "isbn": "123", "reviews": {} },
+    "2": { "title": "Another Book", "author": "Author B", "isbn": "456", "reviews": {} }
+    // ... other books
+  };
 public_users.post("/register", (req,res) => {
   //Write your code here
   return res.status(300).json({message: "Yet to be implemented"});
@@ -29,9 +32,19 @@ public_users.get('/author/:author',function (req, res) {
 });
 
 // Get all books based on title
-public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+
+// Task 13: Get book details based on Title using Promise or async-await
+public_users.get('/title/:title', function (req, res) {
+  const title = req.params.title.toLowerCase();
+
+  // Find books that match the title (case-insensitive)
+  const matchedBooks = Object.values(books).filter(book => book.title.toLowerCase() === title);
+
+  if (matchedBooks.length > 0) {
+    return res.json(matchedBooks);
+  } else {
+    return res.status(404).json({ message: "Book not found with the given title" });
+  }
 });
 
 //  Get book review
